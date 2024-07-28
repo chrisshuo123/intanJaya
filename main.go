d443d0831a005c241f5dbce34aca8b30a083e89e
chrisshuo123 <chr1z100@gmail.com>
2024-07-29 05:25:30 +0700

    push password prototype (sudah dengan if passwordnya salah)

--- main.go
+++ main.go
@@ -9,11 +9,13 @@ import (
 // tpl is a pointer package from Tpl
 var tpl *template.Template
 
+// Inisiasi
 func init() {
 	tpl = template.Must(template.ParseGlob("templates/*.html"))
 }
 
 func main() {
+	// Switcher
 	http.HandleFunc("/", index)
 	http.HandleFunc("/process", processor)
 	if err := http.ListenAndServe(":3030", nil); err != nil {
@@ -21,6 +23,7 @@ func main() {
 	}
 }
 
+// Request
 // Web is about making certain amount of requests
 func index(w http.ResponseWriter, r *http.Request) {
 	tpl.ExecuteTemplate(w, "index.html", nil)
@@ -35,6 +38,27 @@ func processor(w http.ResponseWriter, r *http.Request) {
 	email := r.FormValue("emailUser")
 	password := r.FormValue("passwordUser")
 
+	if password != "bigbears" {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+	//password := r.FormValue("passwordUser")
+
+	/*if password != passwordCheck(password) {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	} else {
+		d := struct {
+			Email    string
+			Password string
+		}{
+			Email:    email,
+			Password: password,
+		}
+
+		tpl.ExecuteTemplate(w, "processor.html", d)
+	}*/
+
 	d := struct {
 		Email    string
 		Password string
@@ -45,3 +69,11 @@ func processor(w http.ResponseWriter, r *http.Request) {
 
 	tpl.ExecuteTemplate(w, "processor.html", d)
 }
+
+/*func passwordCheck(password string, c echo.Context) (bool, error) {
+	// If no password was given, return an error with a message.
+	if password == "bigbears" {
+		return true, nil
+	}
+	return false, nil
+}*/
